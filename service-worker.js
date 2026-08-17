5ba5ecc88b7c3fa5dcc03e783e78020bc2227588
lucasdpr <lucasdepaularafael@gmail.com>
2026-08-17 11:11:24 -0300

    organizacao css

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@
 // usando a copia antiga guardada em cache.
 // ==============================================================
 
-const CACHE_VERSION = "oms-v21";
+const CACHE_VERSION = "oms-v23";
 
 // Arquivos baixados e guardados assim que o app é instalado.
 // (não inclui chamadas de API - essas nunca ficam em cache)
